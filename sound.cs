502049440577bb9f59acaeb155948ed594207a4c
kabbesj <kabbesj@gmail.com>
2017-04-03 23:02:36 -0500

    Add files via upload

---
+++ sound.cs
@@ -0,0 +1,15 @@
+//sound.cs
+//Just a simple consle application that uses the System.Media library to make a sound
+
+using System;
+using System.Media;
+
+public class sound{
+	public static void Main(){
+		
+				//SystemSounds.Question.Play();
+				//SystemSounds.Beep.Play();
+				SystemSounds.Asterisk.Play();
+		
+	}
+}
