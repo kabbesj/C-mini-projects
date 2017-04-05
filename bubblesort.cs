36d20c1aa37b12538e255ae84ae69f65ea4df7e7
kabbesj <kabbesj@gmail.com>
2017-04-05 17:59:47 -0500

    Update bubblesort.cs

--- bubblesort.cs
+++ bubblesort.cs
@@ -6,15 +6,15 @@ using System;
 public class bubblesort{
 	public static void Main(){
 		//int[] arr = { 100, 200, 300, 400, 500, 600, 700, 800, 900 }; /// for integer sort
-		string[] arr = {"detroit lions","green bay packers","chicago bears","minnesota vikings"}; ///for string sort
+		String[] arr = {"detroit lions","green bay packers","chicago bears","minnesota vikings"}; ///for string sort
 
 		//int temp = 0;		///for integer sort
-		string temp = ""; /// for string sort
+		String temp = ""; /// for string sort
 
 		for (int write = 0; write < arr.Length; write++) {			
 			for (int sort = 0; sort < arr.Length - 1; sort++) {				
 				//if (arr[sort] > arr[sort + 1])	/// for integer sort			
-				if (string.Compare(arr[sort], arr[sort+1]) == 1){ ///for string sort				
+				if (String.Compare(arr[sort], arr[sort+1]) == 1){ ///for string sort				
 					temp = arr[sort + 1];
 					arr[sort + 1] = arr[sort];
 					arr[sort] = temp;
